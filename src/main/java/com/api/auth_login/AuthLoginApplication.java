fd726303213b77e4907a37040ff31db98315321b
gabrielfonseca-de <gabriel.fonseca.on@gmail.com>
2025-04-14 20:13:45 -0300

    Beginning of the project

---
+++ src/main/java/com/api/auth_login/AuthLoginApplication.java
@@ -0,0 +1,13 @@
+package com.api.auth_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthLoginApplication.class, args);
+	}
+
+}
