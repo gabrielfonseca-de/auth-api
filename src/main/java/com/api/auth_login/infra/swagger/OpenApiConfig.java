28dc698d104a94b978b11fa17cebf43f1b64510c
gabrielfonseca-de <gabriel.fonseca.on@gmail.com>
2025-05-22 21:49:07 -0300

    Beginning of the project

---
+++ src/main/java/com/api/auth_login/infra/swagger/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.api.auth_login.infra.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Auth API")
+                        .version("1.0")
+                        .description("API de autenticação com Spring Boot, JWT e Java 21"));
+    }
+}
